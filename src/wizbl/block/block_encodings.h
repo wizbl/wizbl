3974d01be66b6a29e97dc079682eda1eebf74296
sj-hong <sj.hong@wizbl.com>
2018-07-13 13:20:15 +0900

    Add logging to provide anchor points when debugging failures.

--- src/wizbl/block/block_encodings.h
+++ src/wizbl/block/block_encodings.h
@@ -163,4 +163,4 @@ protected:
 };
 
 #endif
-                                                                                                                   
+                                                                                                                    
