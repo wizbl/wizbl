876f7afd19d5a418024f6d261a4c86bf5ef61c16
BKCho-Wizbl <bk.cho@wizbl.com>
2018-07-16 17:57:15 +0900

    correcting the array name

--- src/wizbl/consensus/params.h
+++ src/wizbl/consensus/params.h
@@ -75,4 +75,4 @@ enum DeploymentPos {
 }
 
 #endif
-                                                                                                                           
+                                                                                                                            
