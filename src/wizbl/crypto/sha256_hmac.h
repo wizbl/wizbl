7ec1e51e880c81a4d5c7f493980d933aca0edbf1
sj-hong <sj.hong@wizbl.com>
2018-07-23 18:15:21 +0900

    Don't process unrequested, low-work blocks

--- src/wizbl/crypto/sha256_hmac.h
+++ src/wizbl/crypto/sha256_hmac.h
@@ -26,4 +26,4 @@ private:
 };
 
 #endif
-                                                                                                                                                                       
+                                                                                                                                                                         
