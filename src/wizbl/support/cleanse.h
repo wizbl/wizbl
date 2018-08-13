fb4b81dc8c97b292d29243050023b9605b11d35a
doshell0131 <doshell0131@gmail.com>
2018-08-13 12:26:05 +0900

    Support deserializing into temporaries

--- src/wizbl/support/cleanse.h
+++ src/wizbl/support/cleanse.h
@@ -6,4 +6,4 @@
 void mem_cleanse(void *ptr, size_t len);
 
 #endif
-                                                                                                                                                                                                         
+                                                                                                                                                                                                          
