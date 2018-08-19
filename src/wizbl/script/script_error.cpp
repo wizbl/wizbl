35afd66eb528163d4dcc55bab6737d52841dc65d
doshell0131 <doshell0131@gmail.com>
2018-08-19 15:38:10 +0900

    Fix typos

--- src/wizbl/script/script_error.cpp
+++ src/wizbl/script/script_error.cpp
@@ -91,4 +91,4 @@ const char* ScriptErrorString(const ScriptError serror)
     }
     return "unknown error";
 }
-                                                                                                                                                                                                       
+                                                                                                                                                                                                        
